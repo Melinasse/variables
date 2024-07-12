06a338de971b481066c8a0398c86e731a535b66d
Azam4t17 <AyzekMendes@gmail.com>
2024-07-12 21:21:19 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+qty_homework='12'; #qty - quantity (количество)
+qty_time_spent='1.5';
+training_course='Курс:Python';
+average_time=(float(qty_time_spent)/int(qty_homework));
+print(training_course,'всего задач:', qty_homework,',''затрачено часов:', qty_time_spent,',''среднее время выполнения', average_time,'часа.');
